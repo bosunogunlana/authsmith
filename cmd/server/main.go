7946f65392f4c0b4ff0a2d7c2bef24013023f167
bosunogunlana <tunbosunogunlana1@gmail.com>
2026-06-03 16:20:55 -0400

    feat: Setup DB migration boilerplate

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -13,13 +13,16 @@ func main() {
 	config := config.Load()
 	log.Printf("config loaded")
 
-	_, err := database.Connect(config.DatabaseDSN)
+	db, err := database.Connect(config.DatabaseDSN)
 	if err != nil {
 		log.Fatalf("failed to connect to db: %v", err)
 	}
 
-	mux := httpserver.ServerMux()
+	if err := database.RunMigration(db, "migrations"); err != nil {
+		log.Fatalf("db migration failed: %v", err)
+	}
 
+	mux := httpserver.ServerMux()
 	if err := http.ListenAndServe(":"+config.Port, mux); err != nil {
 		log.Fatalf("server failed to start: %v", err)
 	}
---
+++ internal/database/migrate.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"database/sql"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func RunMigration(db *sql.DB, dirName string) error {
+	directory, err := os.ReadDir(dirName)
+	if err != nil {
+		return err
+	}
+
+	migrationFiles := []string{}
+	for _, entry := range directory {
+		fileName := entry.Name()
+		if strings.HasSuffix(fileName, ".sql") {
+			migrationFiles = append(migrationFiles, fileName)
+		}
+	}
+
+	for _, migrationFile := range migrationFiles {
+		filePath := filepath.Join(dirName, migrationFile)
+		file, err := os.ReadFile(filePath)
+		if err != nil {
+			return err
+		}
+
+		_, err = db.Exec(string(file))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
